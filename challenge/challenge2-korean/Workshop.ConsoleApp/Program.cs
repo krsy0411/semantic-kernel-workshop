abceed3bfa0fc26835e40838ae6f832d17ef244a
PANdas <krsy0411@naver.com>
2025-02-11 01:33:19 +0900

    도전과제 : 마지막 제외

---
+++ challenge/challenge2-korean/Workshop.ConsoleApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.ClientModel;
+
+using Azure;
+using Azure.AI.OpenAI;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+using OpenAI;
+
+var config = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                 .AddUserSecrets<Program>()
+                 .Build();
+
+var builder = Kernel.CreateBuilder();
+if (string.IsNullOrWhiteSpace(config["Azure:OpenAI:Endpoint"]!) == false)
+{
+    var client = new AzureOpenAIClient(
+        new Uri(config["Azure:OpenAI:Endpoint"]!),
+        new AzureKeyCredential(config["Azure:OpenAI:ApiKey"]!));
+
+    builder.AddAzureOpenAIChatCompletion(
+                deploymentName: config["Azure:OpenAI:DeploymentName"]!,
+                azureOpenAIClient: client);
+}
+else
+{
+    var client = new OpenAIClient(
+        credential: new ApiKeyCredential(config["GitHub:Models:AccessToken"]!),
+        options: new OpenAIClientOptions { Endpoint = new Uri(config["GitHub:Models:Endpoint"]!) });
+
+    builder.AddOpenAIChatCompletion(
+                modelId: config["GitHub:Models:ModelId"]!,
+                openAIClient: client);
+}
+var kernel = builder.Build();
+
+var background = "나는 맛있는 음식과 야외활동을 좋아해.";
+var plugins = kernel.CreatePluginFromPromptDirectory(Path.Combine(AppContext.BaseDirectory, "../../..", "Plugins"));
+
+var input = default(string);
+var message = default(string);
+while (true)
+{
+    Console.WriteLine("여러분이 방문 중인 도시를 알려주세요.");
+    Console.Write("유저: ");
+    input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        break;
+    }
+
+    Console.Write("어시스턴트: ");
+
+    var arguments = new KernelArguments()
+    {
+        { "city", input },
+        { "background", background }
+    };
+
+
+    var response = kernel.InvokeStreamingAsync(plugins["TravelAgent"], arguments);
+    await foreach (var content in response)
+    {
+        await Task.Delay(20);
+        message += content;
+        Console.Write(content);
+    }
+    Console.WriteLine();
+
+    Console.WriteLine();
+}
---
+++ challenge/challenge2-weather/Workshop.ConsoleApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.ClientModel;
+
+using Azure;
+using Azure.AI.OpenAI;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+using OpenAI;
+// using Workshop.ConsoleApp.Plugins.BookingAgent;
+using Workshop.ConsoleApp.Plugins.WeatherBot;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var config = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                 .AddUserSecrets<Program>()
+                 .Build();
+
+var builder = Kernel.CreateBuilder();
+if (string.IsNullOrWhiteSpace(config["Azure:OpenAI:Endpoint"]!) == false)
+{
+    var client = new AzureOpenAIClient(
+        new Uri(config["Azure:OpenAI:Endpoint"]!),
+        new AzureKeyCredential(config["Azure:OpenAI:ApiKey"]!));
+
+    builder.AddAzureOpenAIChatCompletion(
+                deploymentName: config["Azure:OpenAI:DeploymentName"]!,
+                azureOpenAIClient: client);
+}
+else
+{
+    var client = new OpenAIClient(
+        credential: new ApiKeyCredential(config["GitHub:Models:AccessToken"]!),
+        options: new OpenAIClientOptions { Endpoint = new Uri(config["GitHub:Models:Endpoint"]!) });
+
+    builder.AddOpenAIChatCompletion(
+                modelId: config["GitHub:Models:ModelId"]!,
+                openAIClient: client);
+}
+var kernel = builder.Build();
+
+// kernel.Plugins.AddFromType<TrainBookingPlugin>("TrainBooking");
+// var settings = new PromptExecutionSettings()
+// {
+//     FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+// };
+// var history = new ChatHistory();
+// history.AddSystemMessage("The year is 2025 and the current month is February");
+// var service = kernel.GetRequiredService<IChatCompletionService>();
+
+// Console.WriteLine("I'm a train booking assistant. How can I help you today?");
+
+kernel.Plugins.AddFromType<WeatherPlugin>("Weather");
+var settings = new PromptExecutionSettings()
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+var history = new ChatHistory();
+var service = kernel.GetRequiredService<IChatCompletionService>();
+
+Console.WriteLine("I'm a weather assistant. How can I help you today?");
+
+var input = default(string);
+var message = default(string);
+while (true)
+{
+    Console.Write("User: ");
+    input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        break;
+    }
+
+    Console.Write("Assistant: ");
+
+    history.AddUserMessage(input);
+
+    var response = service.GetStreamingChatMessageContentsAsync(history, settings, kernel);
+    await foreach (var content in response)
+    {
+        await Task.Delay(20);
+        message += content;
+        Console.Write(content);
+    }
+    history.AddAssistantMessage(message!); 
+    Console.WriteLine();
+
+    Console.WriteLine();
+}
